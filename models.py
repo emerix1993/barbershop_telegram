abdf39764ca1dc337ba72e7d2e4690176201ca00
emerix1993 <104599528+emerix1993@users.noreply.github.com>
2023-08-18 20:19:04 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,10 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship, sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///barbershop_telegram.db', echo=True)
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres', echo=True)
 
 class User(Base):
     __tablename__ = 'users'
@@ -21,10 +24,10 @@ class User(Base):
     date = Column(DateTime)
     time = Column(String)
 
-    Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    def __repr__(self):
-        return f'User(id={self.chat_id}, first_name={self.first_name}, last_name={self.last_name}, phone={self.phone}, procedure={self.procedure}, date={self.date}, time={self.time})'
+Session = sessionmaker(bind=engine)
+session = Session()
+def __repr__(self):
+    return f'User(id={self.chat_id}, first_name={self.first_name}, last_name={self.last_name}, phone={self.phone}, procedure={self.procedure}, date={self.date}, time={self.time})'
 
