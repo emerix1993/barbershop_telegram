5ff106b8ad665ae1ddb82608159d992935641fd1
aleksandrduhin <emerix@ukr.net>
2023-08-25 03:05:23 +0300

    -Work with databases-

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from models import User
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, \
     ReplyKeyboardRemove
-from models import User, Base, engine
+from models import User, Base, engine, session
 
 from barbershop_telegram import config
 from config import BOT_TOKEN
@@ -22,9 +22,11 @@ bot = telebot.TeleBot(config.BOT_TOKEN)
 procedures = ['Стрижка', 'Гоління', 'Комплекс', 'Написати адміністратору']
 
 
+phone_number = None  #Global variable to store the phone number
+
 @bot.message_handler(commands=['start'])
 def phone(message):
-    global chat_id
+    global phone_number
     chat_id = message.chat.id
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button_phone = types.KeyboardButton(text="Відправити номер телефону", request_contact=True)
@@ -35,6 +37,9 @@ def phone(message):
 @bot.message_handler(content_types=['contact'])
 def contact(message):
     if message.contact is not None:
+        global phone_number
+        phone_number = message.contact.phone_number
+        print("Received phone number:", phone_number)
         print(message.contact)
         inline_markup = telebot.types.InlineKeyboardMarkup(row_width=1)
         for procedure in procedures:
@@ -46,13 +51,22 @@ def contact(message):
 def process_procedure(callback_query, process_date_time=str):
     procedure = callback_query.data
     bot.send_message(callback_query.from_user.id, f"Ви обрали процедуру: {procedure}")
-    new_user = User(chat_id=callback_query.message.chat.id, procedure=procedure, first_name=callback_query.from_user.first_name, last_name=callback_query.from_user.last_name, phone=callback_query.from_user.contact.phone_number)
-    session.add(new_user)
-    session.commit()
-    bot.send_message(callback_query.from_user.id,
-                     "Введіть дату та час візиту у форматі ДД-ММ та час(приклад -> 19.05 15:00)")
-    bot.register_next_step_handler(callback_query.message, process_date_time)
+    global phone_number
+    if phone_number:
+        new_user = User(
+            chat_id=callback_query.from_user.id,
+            first_name=callback_query.from_user.first_name,
+            last_name=callback_query.from_user.last_name,
+            phone=phone_number,
+            procedure=procedure
+        )
+        session.add(new_user)
+        session.commit()
+        bot.send_message(callback_query.from_user.id,
+                         "Введіть дату та час візиту у форматі ДД-ММ та час(приклад -> 19.05 15:00)")
+        bot.register_next_step_handler(callback_query.message, process_date_time)
     bot.answer_callback_query(callback_query.id)
+
 
 
 print("Telegram Support Bot started...")
